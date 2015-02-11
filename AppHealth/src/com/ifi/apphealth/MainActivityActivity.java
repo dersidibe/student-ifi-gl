e468972c9b48c35dc84aa38b7325fb917a353f27
SIDIBE Der <sidegsy@gmail.com>
2015-02-11 17:19:47 +0700

    Mise en ligne du projet AppHealth

---
+++ AppHealth/src/com/ifi/apphealth/MainActivityActivity.java
@@ -0,0 +1,14 @@
+package com.ifi.apphealth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivityActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
+
+}
