04ad591d23665f17ea770b5c6b13f37995f94280
SIDIBE Der <sidegsy@gmail.com>
2015-02-26 03:59:00 +0700

    mise du parser

---
+++ AppHealth/app/src/main/java/com/apphealth/ifi/adapter/MyAdapter.java
@@ -0,0 +1,65 @@
+package com.apphealth.ifi.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import com.apphealth.ifi.appheath.R;
+import com.apphealth.ifi.beans.Structure;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends BaseAdapter{
+
+    private Context context;
+    private ArrayList<Structure> mData  = new ArrayList<Structure>();
+
+
+    public MyAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+
+        return this.mData.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+
+        return mData.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+
+        return pos;
+    }
+
+    @Override
+    public View getView(int pos, View vue, ViewGroup parent) {
+
+        Structure structure = mData.get(pos);
+        if(vue == null){
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            vue = inflater.inflate(R.layout.liste, null);
+        }
+
+        StructureListView structureView = new StructureListView(context, structure);
+
+        return structureView;
+    }
+
+    public ArrayList<Structure> getListeStructure(){
+
+        return mData;
+    }
+
+    public void setListeStructure(ArrayList<Structure> art){
+
+        this.mData = art;
+    }
+}
--- AppHealth/app/src/main/java/com/apphealth/ifi/adapter/StructureListView.java
+++ AppHealth/app/src/main/java/com/apphealth/ifi/adapter/StructureListView.java
@@ -1,4 +1,4 @@
-package com.apphealth.ifi.asynctask;
+package com.apphealth.ifi.adapter;
 
 
 import android.widget.LinearLayout;
@@ -23,13 +23,15 @@ public class StructureListView extends LinearLayout{
     private TextView distanceStructure;
     private ImageView logoStructure;
     Context context;
+    private Structure structure;
 
     public StructureListView(Context context, Structure structure) {
         super(context);
 
         this.context = context;
         initLayout(context);
-
+        initComposants();
+        setArticle(structure);
     }
 
     private void initLayout(Context context){
@@ -38,5 +40,28 @@ public class StructureListView extends LinearLayout{
         inflater.inflate(R.layout.liste, this, true);
     }
 
+    private void initComposants(){
 
+        this.nomStructure = (TextView) this.findViewById(R.id.name);
+        this.distanceStructure = (TextView) this.findViewById(R.id.distance);
+        this.logoStructure = (ImageView) this.findViewById(R.id.list_image);
+
+    }
+    private void initContenuComposants(Structure structure){
+
+        this.nomStructure.setText(structure.getNom());
+        this.distanceStructure.setText(structure.getDistance()+" mètres");
+    }
+
+    public Structure getStructure(){
+        return structure;
+    }
+
+    public void setArticle(Structure structure){
+
+        this.structure = structure;
+
+        initContenuComposants(structure);
+
+    }
 }
